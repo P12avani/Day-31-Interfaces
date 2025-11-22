5a904064deb2b5873cc32f036e89d0571b3377bf
pavani polipalli <polipallipavani54@gmail.com>
2025-11-22 21:43:32 +0530

    Fortune

---
+++ Fortuner.java
@@ -0,0 +1,32 @@
+package com.codegnan.oop.interfaces;
+
+public class Fortuner implements Car {
+
+	@Override
+	public void accelerate(int points) {
+	if(points<=100&&points>=10)	{
+		System.out.println("Fortuner Speed is:" +points);
+	}
+		
+	}
+
+	@Override
+	public void turnDirection(String dir) {
+		if(dir.equalsIgnoreCase("left")) {
+			System.out.println("Fortuner is turning left");
+		}if(dir.equalsIgnoreCase("right")) {
+			System.out.println("Fortuner is turning right");
+		}
+		
+	}
+
+	@Override
+	public void stop() {	
+		System.out.println("Fortuner has stopped");
+	}
+	public void fourWheel() {
+		System.out.println("Fortuner engaged with four wheels");
+	}
+	
+
+}
