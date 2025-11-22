d0649b74d3f60691c89c92f97291d9808431f0c3
pavani polipalli <polipallipavani54@gmail.com>
2025-11-22 21:57:05 +0530

    Main1

---
+++ Main1.java
@@ -0,0 +1,12 @@
+package com.codegnan.oop.interfaces;
+import java.util.Scanner;
+public class Main1 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		scanner.nextLine();
+		Circle c=new Circle();
+		System.out.println(c.getName());
+	}
+	
+
+}
